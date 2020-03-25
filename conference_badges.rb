5915b2e87ece8510d4030e972cd8e520ae73dcd3
Gilberto Ocampo <ocampo@gate.net>
2020-03-25 11:34:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  batch_badge_creator.each do |name| 
+  badges =[]
+  attendees.each { |name| badges <<  
     "Hello, my name is #{name}."
+  badges 
   end 
 end
