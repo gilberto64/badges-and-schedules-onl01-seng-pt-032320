82d11b3f9ecdef7a2f15c8f2d76c9dc3e2893f8c
Gilberto Ocampo <ocampo@gate.net>
2020-03-25 12:06:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,11 +10,11 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  # room = 1
-  # room_assignments []
-  # attendees.each do |name| room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"  
-  # room += 1
+  room = 1
+  room_assignments []
+  attendees.each do |name| room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"  
+  room += 1
   
-# end 
-# room_assignments
+  end 
+  room_assignments
 end
